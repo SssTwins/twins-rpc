ec29a954f8153f79acc4f2b817ddd3fe9bb7554c
songhongbao <songhongbao@uino.com>
2023-07-15 18:02:12 +0800

    feat: 增加示例模块

---
+++ rpc-example/src/main/java/pers/twins/rpc/example/server/ExampleServer.java
@@ -0,0 +1,14 @@
+package pers.twins.rpc.example.server;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author twins
+ * @date 2023-07-15 16:10:35
+ */
+@Slf4j
+public class ExampleServer {
+    public static void main(String[] args) {
+        log.info("Hello world!");
+    }
+}
