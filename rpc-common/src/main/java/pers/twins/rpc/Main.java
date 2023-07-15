4780ff846a8485745334ec11132fc37d1476731d
songhongbao <songhongbao@uino.com>
2023-07-15 15:20:59 +0800

    feat: Project initialization

---
+++ rpc-common/src/main/java/pers/twins/rpc/Main.java
@@ -0,0 +1,11 @@
+package pers.twins.rpc;
+
+/**
+ * @author twins
+ * @date 2023/07/15 15:10:58
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
