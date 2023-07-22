21817f46f84859b59b271963bc93e7cc2d790666
songhongbao <songhongbao@uino.com>
2023-07-22 11:32:32 +0800

    feat: 增加自定数据包结构注释

--- rpc-common/src/main/java/pers/twins/rpc/common/remoting/transport/RpcProtocolConstants.java
+++ rpc-common/src/main/java/pers/twins/rpc/common/remoting/transport/RpcProtocolConstants.java
@@ -4,6 +4,17 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 /**
+ * 请求头结构如下：
+ * 4B magic code（魔法数）
+ * 1B version（版本）
+ * 4B full length（消息长度）
+ * 1B messageType（消息类型）
+ * 1B compress（压缩类型）
+ * 1B codec（序列化类型）
+ * 4B requestId（请求的Id）
+ * 总计16byte
+ * body（object类型数据）
+ *
  * @author twins
  * @date 2023-07-17 20:34:18
  */
